3a58046603938c50c01594b3780759a557b58898
Niko Strijbol <strijbol.niko@gmail.com>
2023-10-17 13:38:51 +0200

    Add files

--- flake.nix
+++ flake.nix
@@ -13,7 +13,63 @@
   outputs = { self, nixpkgs, devshell, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ devshell.overlays.default ]; };
+        pkgs = import nixpkgs { inherit system; overlays = [ devshell.overlays.default ]; config.allowUnfree = true; };
+        ugent2016 = pkgs.stdenvNoCC.mkDerivation (finalAttrs: rec {
+            pname = "ugent2016";
+            version = "0.9.0";
+            passthru = {
+              pkgs = [ finalAttrs.finalPackage ];
+              tlDeps = with pkgs.texlive; [
+                etoolbox
+                kvoptions
+                xstring
+                auxhook
+                translations
+                fontspec
+                pgf
+                textcase
+                graphics
+                geometry
+                setspace
+                ulem
+              ];
+              tlType = "run";
+            };
+
+            src = pkgs.fetchurl {
+              url = "https://github.com/niknetniko/ugent2016/releases/download/${version}/ugent2016.zip";
+              hash = "sha256-3HNdJLpwBKOZIpqma+A9E4rvTl3eQMGn1g4kRD4CTfM=";
+            };
+
+            nativeBuildInputs = [ pkgs.unzip ];
+
+            sourceRoot = ".";
+
+            dontConfigure = true;
+            dontBuild = true;
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out
+              unzip ugent2016.tds -d $out
+
+              runHook postInstall
+            '';
+
+            dontFixup = true;
+
+            meta = with pkgs.lib; {
+              description = "Styles for UGent";
+              license = licenses.unfreeRedistributable;
+              maintainers = [ ];
+              platforms = platforms.all;
+            };
+        });
+        latex_with_ugent = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-full;
+          inherit ugent2016;
+        };
       in
       {
         devShells = rec {
@@ -21,14 +77,14 @@
           site = pkgs.devshell.mkShell {
             name = "doctoraat";
             packages = with pkgs; [
-              texlive.combined.scheme-full
+              latex_with_ugent
               gnumake
               inkscape
             ];
             env = [
               {
                 name = "TEXLIVE_HOME";
-                eval = "${pkgs.texlive.combined.scheme-full}";
+                eval = "${latex_with_ugent}";
               }
             ];
           };
