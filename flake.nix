c0817070bb67177c44a32c3dc0dd3136d604bf19
Niko Strijbol <strijbol.niko@gmail.com>
2024-05-21 16:22:06 +0200

    Add cover to flake

--- flake.nix
+++ flake.nix
@@ -85,49 +85,6 @@
               platforms = platforms.all;
             };
         };
-        tikz-uml = pkgs.stdenvNoCC.mkDerivation rec {
-          pname = "tikz-uml";
-          version = "v1.0b-2013-02-01";
-          outputs = ["tex" "texdoc" "out"];
-
-          passthru.tlDeps = with pkgs.texlive; [
-            pgf
-            xstring
-            tools
-            pgfopts
-          ];
-
-          src = pkgs.fetchurl {
-            url = "https://perso.ensta-paris.fr/~kielbasi/tikzuml/var/files/src/tikzuml-${version}.tbz";
-            hash = "sha256-dKF5ssMqDYKuqPKGHXAQ4k42LlvnosVrbDbVQblgC20=";
-          };
-
-          dontConfigure = true;
-          dontBuild = true;
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p "$out"
-            path="$tex/tex/latex/tikz-uml"
-            mkdir -p "$path"
-            cp *.{cls,def,clo,sty} "$path/"
-            cp *.{cls,def,clo,sty} "$out/"
-
-            path="$texdoc/doc/tex/latex/tikz-uml"
-            mkdir -p "$path"
-            cp doc/*.pdf "$path/"
-
-            runHook postInstall
-          '';
-
-          meta = with pkgs.lib; {
-            description = "TikZ-UML";
-            license = licenses.unfreeRedistributable;
-            maintainers = [ ];
-            platforms = platforms.all;
-          };
-      };
         latex_with_ugent = pkgs.texliveFull.withPackages (ps: with ps; [
 #          luatex
 #          luatexbase
@@ -200,9 +157,9 @@
 #          };
         };
         packages = rec {
-          default = document;
-          document = pkgs.stdenvNoCC.mkDerivation {
-            name = "doctoraat";
+          default = book;
+          book = pkgs.stdenvNoCC.mkDerivation {
+            name = "book";
             src = ./src;
             nativeBuildInputs = with pkgs; [
               which
@@ -228,8 +185,35 @@
             '';
             installPhase = ''
               mkdir -p $out;
-              cp $BUILD_DIR/main.pdf $out/doctoraat.pdf
-              cp $BUILD_DIR/main.log $out/doctoraat.log
+              cp $BUILD_DIR/main.pdf $out/book.pdf
+              cp $BUILD_DIR/main.log $out/book.log
+            '';
+          };
+          cover = pkgs.stdenvNoCC.mkDerivation {
+            name = "cover";
+            src = ./src/cover;
+            nativeBuildInputs = with pkgs; [
+              latex_with_ugent
+              ugent-panno
+              source-serif
+              source-sans
+              source-code-pro
+            ];
+            phases = ["unpackPhase" "configurePhase" "buildPhase" "installPhase"];
+            configurePhase = ''
+              export TEXLIVE_HOME="${latex_with_ugent}"
+              export OSFONTDIR="${font_dir}"
+              export BUILD_DIR=$TMPDIR/build
+              export TEXMFHOME=$TMPDIR/.cache
+              export TEXMFVAR=$TMPDIR/.cache/texmf-var
+            '';
+            buildPhase = ''
+              latexmk -diagnostics -f -pdf -lualatex cover.tex -output-directory=$BUILD_DIR -interaction=nonstopmode
+            '';
+            installPhase = ''
+              mkdir -p $out;
+              cp $BUILD_DIR/cover.pdf $out/cover.pdf
+              cp $BUILD_DIR/cover.log $out/cover.log
             '';
           };
         };
